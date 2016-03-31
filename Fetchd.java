0ada70277f08f4c5be4bb6174cc7bab965d73b44
Taylor Dohmen <tdohmen@mail.umw.edu>
2016-03-31 19:19:03 -0400

    Began writing infrastructure for the UserManager class

--- Fetchd.java
+++ Fetchd.java
@@ -1,6 +1,6 @@
 public class Fetchd {
 	
 	public static void main(String[] args) {
-	
+
 	}
 }
--- UserManager.java
+++ UserManager.java
@@ -1,3 +1,33 @@
+import java.io.*;
+
 public class UserManager {
 	
+	public void registerUser(String[] userInfo) {
+
+	}
+
+	public void loginUser(String username, String password) {
+
+	}
+
+	public void logoutUser(String username, String password) {
+
+	}
+
+	//This method doesn't do anything yet
+	public String[][] getAllUsers() {
+		FileInputStream file = new FileInputStream("UserInfo.txt");
+		Scanner in = new Scanner(file);
+		while (in.hasNext()) {
+
+		}
+	}
+
+	public String[] getUserInfo(String username) {
+
+	} 
+
+	public String[] getSubscribers(String username) {
+
+	}
 }
