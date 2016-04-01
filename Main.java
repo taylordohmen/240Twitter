c0905f478348e9a1fa4961279a129342954a1203
rcf0900 <abeamer@mail.umw.edu>
2016-04-01 10:21:33 -0400

    Started menu, questions about logging in

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.*;
+/*
+*
+*
+*
+*/
 public class Main {
 	
 	public static void main(String[] args) {
-	
+		final Scanner input = new Scanner(System.in);
+		
+		//Menu displays
+		System.out.println("Welcome to Fetch'd!");
+		System.out.println("Select an action: \n" +
+		"1)Register a new user\n" +
+		"2)Log in\n" );					
 	}
 }
