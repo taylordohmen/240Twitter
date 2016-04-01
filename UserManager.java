4b5083abfd50576a4fed930681b4af5bdca367c1
Taylor Dohmen <tdohmen@mail.umw.edu>
2016-04-01 01:29:13 -0400

    Added in getAllUsers() method which retrieves user information for every registered user of the system.

--- UserManager.java
+++ UserManager.java
@@ -1,33 +1,64 @@
+
 import java.io.*;
+import java.util.Scanner;
 
 public class UserManager {
-	
-	public void registerUser(String[] userInfo) {
 
-	}
+    private static final int NUMFIELDS = 15;
 
-	public void loginUser(String username, String password) {
+    public void registerUser(String[] userInfo) {
 
-	}
+    }
 
-	public void logoutUser(String username, String password) {
+    public void loginUser(String username, String password) {
 
-	}
+    }
 
-	//This method doesn't do anything yet
-	public String[][] getAllUsers() {
-		FileInputStream file = new FileInputStream("UserInfo.txt");
-		Scanner in = new Scanner(file);
-		while (in.hasNext()) {
+    public void logoutUser(String username, String password) {
 
-		}
-	}
+    }
 
-	public String[] getUserInfo(String username) {
+    //numUsers is the number of registered users on file
+    //each user must have all info on a single line
+    public static String[][] getAllUsers(int numUsers) {
 
-	} 
+        String[][] allUsers = new String[numUsers][NUMFIELDS];
+        String[] allFields = new String[NUMFIELDS];
+        int placeCount = 0;
 
-	public String[] getSubscribers(String username) {
+        try (FileInputStream fStream = new FileInputStream("UserInfo.txt")) {
 
-	}
+            Scanner in = new Scanner(fStream);
+            StringBuilder currentField = new StringBuilder("");
+
+            for (int j = 0; j < numUsers; j++) {
+
+                String line = in.next();
+
+                for (int i = 0; i < line.length(); i++) {
+                    char currentChar = line.charAt(i);
+                    if (currentChar == ',') {
+                        allFields[placeCount] = currentField.toString();
+                        placeCount++;
+                        currentField = new StringBuilder("");
+                    } else {
+                        currentField.append(currentChar);
+                    }
+                }
+                
+                allUsers[j] = allFields;
+            }
+        } catch (IOException e) {}
+
+        return allUsers;
+    }
+
+//
+//	public String[] getUserInfo(String username) {
+//
+//	} 
+//
+//	public String[] getSubscribers(String username) {
+//
+//	}
 }
