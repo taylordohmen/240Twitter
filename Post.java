5b0be9690d465282e4d99d672021bee2c701c407
kfrance100 <kjfrance27@aol.com>
2016-04-03 16:23:55 -0400

    added data members, created basic constructor, comments on processes (need to be addressed)

--- Post.java
+++ Post.java
@@ -1,3 +1,22 @@
+import java.util.*;
 public class Post {
-	
+	private int postID; //how will this be stored and incremented?
+        private int privacyLevel; //how is this determined and added to the post? Is 1 admin, 2 normal, 3 dm?
+        private String postContents;
+        //private String postAuthor = User.getUsername();
+        private String locationTag;
+        private String hashtag; //need to search through postContents for these and keep them separate
+        private Date date;
+        
+        public Post() {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Input a message to post: ");
+            postContents = in.nextLine();
+            System.out.println("Input location: ");
+            locationTag = in.nextLine();
+            
+            //postID++;
+        }
+        
+        
 }
